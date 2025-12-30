5329f762427fb0529796077e80ce32494eaa3bd0
silasenapong <sila_sanapong@cmu.ac.th>
2025-12-30 17:04:24 +0700

    Lab10

---
+++ lab/Lab_10/Lab10_1.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int count[5] = {0};
+    int i = 1;
+    char grade;
+
+    cout << "Please input grade of each student (A-F) or input 0 to exit." << endl;
+
+    do
+    {
+        cout << "Student [" << i << "]: ";
+        cin >> grade;
+
+        if (grade == '0')
+        {
+            break;
+        }
+
+        if (grade == 'A')
+        {
+            count[0]++;
+            i++;
+        }
+        else if (grade == 'B')
+        {
+            count[1]++;
+            i++;
+        }
+        else if (grade == 'C')
+        {
+            count[2]++;
+            i++;
+        }
+        else if (grade == 'D')
+        {
+            count[3]++;
+            i++;
+        }
+        else if (grade == 'F')
+        {
+            count[4]++;
+            i++;
+        }
+        else
+        {
+            cout << "Wrong input. Please input again." << endl;
+        }
+
+    } while (true);
+
+    cout << "In total " << i - 1 << " students." << endl;
+    cout << "A = " << count[0] << ", ";
+    cout << "B = " << count[1] << ", ";
+    cout << "C = " << count[2] << ", ";
+    cout << "D = " << count[3] << ", ";
+    cout << "F = " << count[4];
+
+    return 0;
+}
---
+++ lab/Lab_10/Lab10_2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream source;
+    ofstream dest;
+    source.open("cheerbook.txt");
+    dest.open("cheerbook_copy.txt");
+
+    string text;
+
+    dest << "-------------------- BOOM ---------------------" << endl;
+
+    while (getline(source, text))
+    {
+        dest << text << endl;
+    }
+
+    dest << "-------------------- HA!! ---------------------";
+
+    source.close();
+    dest.close();
+    return 0;
+}
