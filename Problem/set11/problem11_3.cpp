fc9a2e0ebf1c3e7c909e82bd00f8ce9451c37517
silasenapong <sila_sanapong@cmu.ac.th>
2026-01-05 19:55:52 +0700

    add pic

--- Problem/set11/problem11_3.cpp
+++ Problem/set11/problem11_3.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <cmath>
 
-void cal1(int n)
+int cal1(int n)
 {
+
+    int sum;
+
+    if (n == 1)
+    {
+    }
+    else
+    {
+        // n-1
+        return cal1(pow(2, n)) + cal1(pow(2, n - 1));
+    }
 }
 
 int main()
