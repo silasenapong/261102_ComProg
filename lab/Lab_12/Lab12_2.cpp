ed6ccc81b4905cb95d531cb8bd0154b8bd0fda00
silasenapong <sila_sanapong@cmu.ac.th>
2026-01-07 10:54:01 +0700

    lab12

---
+++ lab/Lab_12/Lab12_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+long long int fibonacci(int x);
+
+int main()
+{
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
+}
+
+long long int fibonacci(int x)
+{
+    if (x == 0)
+    {
+        return 0;
+    }
+    else if (x == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return fibonacci(x - 1) + fibonacci(x - 2);
+    }
+}
