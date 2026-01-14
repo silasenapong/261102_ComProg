dd7ed58af70efb627bf11668ce1f7408dfe59189
silasenapong <sila_sanapong@cmu.ac.th>
2026-01-14 15:24:36 +0700

    lab14_1

---
+++ lab/Lab_14/lab14_1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void show(T d[], int N);
+
+template <typename T>
+void insertionSort(T d[], int N);
+
+int main()
+{
+    int a[10] = {12, 25, 30, 44, 2, 0, 4, 7, 55, 25};
+    cout << "Input Array:";
+    for (int i = 0; i < 10; i++)
+        cout << a[i] << " ";
+    cout << "\n\n";
+
+    insertionSort(a, 10);
+
+    cout << "\nSorted Array:";
+    for (int i = 0; i < 10; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
+
+template <typename T>
+void show(T d[], int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << d[i] << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void insertionSort(T d[], int N)
+{
+    for (int i = 1; i < N; i++)
+    {
+        int j = i - 1;
+    }
+}
